ff80998efd5f115efad1353b5cd640a60b9d0e82
JuanK <camiloalzatebedoya15@gmail.com>
2025-12-02 17:54:37 -0500

    Prueba websocket

--- src/main/java/com/sopadeletras/config/WebSocketConfig.java
+++ src/main/java/com/sopadeletras/config/WebSocketConfig.java
@@ -10,13 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/list");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket");
+        registry.addEndpoint("/websocket")
+            .setAllowedOrigins("*");
     }
 
 }
